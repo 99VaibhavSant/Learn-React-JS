b68786fff37633e8fca5e3a0795e71b8a399fb14
99vaibhavsant <99vaibhavsant@gmail.com>
2026-02-13 18:44:26 +0530

    Small activity using prop

---
+++ 10_Activity/src/App.jsx
@@ -0,0 +1,12 @@
+import MsgBox from "./MsgBox.jsx"
+function App() {
+
+  return (
+    <>
+  <MsgBox name="Vaibhav" textcolor = "red"/>
+  <MsgBox name="Ram" textcolor = "yellow"/>
+    </>
+  )
+}
+
+export default App
---
+++ 10_Activity/src/MsgBox.jsx
@@ -0,0 +1,7 @@
+export default function MsgBox({name , textcolor}){
+    return(
+            <div>
+        <h1 style={{color:textcolor}}>Name is {name}</h1>
+        </div>
+    )
+}
